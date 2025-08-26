d08aba9e9c99f490b6bde29da74e719f2d878d6f
NIKHIL <65222469+NikhilNGY@users.noreply.github.com>
2025-08-26 17:37:14 +0530

    Update and rename config (1).py to config.py

--- config.py
+++ config.py
@@ -20,26 +20,26 @@ def is_enabled(value, default=False):
     return default
 
 # ---------- Bot Information ----------
-API_ID = int(environ.get("API_ID", "4770590"))
-API_HASH = environ.get("API_HASH", "e33bf9032335b874acb9c6406f044836")
-BOT_TOKEN = environ.get("BOT_TOKEN", "7184578061:AAEDa_nHSiEzRXFFPIM3UnLkLDOgRhkiXig")
+API_ID = int(environ.get("API_ID", "2468192"))
+API_HASH = environ.get("API_HASH", "4906b3f8f198ec0e24edb2c197677678")
+BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
 # Pictures for /start (space-separated list). Safe if empty.
 _default_pics = (
-    "https://telegra.ph/file/eb119179b4d2a13e71163.jpg "
-    "https://telegra.ph/file/dc5a1a49c2786685ff97a.jpg "
-    "https://telegra.ph/file/1a9519a68c4d45ac9455a.jpg "
-    "https://telegra.ph/file/e5a3d6969f2082eecc3c1.jpg "
-    "https://telegra.ph/file/57d6d774cf4baf2de5968.jpg"
+    "https://telegra.ph/file/84e99f37a84ea85be1bd5.jpg "
+    "https://telegra.ph/file/ea10bde858558928aea10.jpg "
+    "https://telegra.ph/file/13cc320175d6fc4ccbace.jpg "
+    "https://telegra.ph/file/9107275ff1d9b71db6bec.jpg "
+    "https://telegra.ph/file/4466d37d43f5703516f74.jpg"
 )
 PICS = [p for p in environ.get("PICS", _default_pics).split() if p] or _default_pics.split()
 
 ADMINS = [
     int(a) if id_pattern.search(a) else a
-    for a in environ.get("ADMINS", "1654334233").split()
+    for a in environ.get("ADMINS", "2068233407").split()
 ]
 
-BOT_USERNAME = environ.get("BOT_USERNAME", "VenomStoneFileStoreBot")  # without @
+BOT_USERNAME = environ.get("BOT_USERNAME", "File_StoreRobot")  # without @
 PORT = int(environ.get("PORT", "8080"))
 
 # ---------- Clone Info ----------
@@ -48,16 +48,16 @@ CLONE_MODE = is_enabled(environ.get("CLONE_MODE"), False)
 # If Clone Mode is True, fill these:
 CLONE_DB_URI = environ.get(
     "CLONE_DB_URI",
-    "mongodb+srv://kajaki7757:kGDRiPGNX691vlCL@cluster0.ylww9nv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    "",
 )
-CDB_NAME = environ.get("CDB_NAME", "cluster0")
+CDB_NAME = environ.get("CDB_NAME", "Filter1")
 
 # ---------- Database Information ----------
 DB_URI = environ.get(
     "DB_URI",
-    "mongodb+srv://kajaki7757:kGDRiPGNX691vlCL@cluster0.ylww9nv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    "",
 )
-DB_NAME = environ.get("DB_NAME", "cluster0")
+DB_NAME = environ.get("DB_NAME", "Filter1")
 
 # ---------- Auto Delete ----------
 AUTO_DELETE_MODE = is_enabled(environ.get("AUTO_DELETE_MODE"), True)
@@ -65,7 +65,7 @@ AUTO_DELETE = int(environ.get("AUTO_DELETE", "30"))          # minutes
 AUTO_DELETE_TIME = int(environ.get("AUTO_DELETE_TIME", "1800"))  # seconds
 
 # ---------- Channel / Logs ----------
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002113810572"))
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1001693006436"))
 
 # ---------- File Caption ----------
 CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", f"{script.CAPTION}")
@@ -75,13 +75,13 @@ BATCH_FILE_CAPTION = environ.get("BATCH_FILE_CAPTION", CUSTOM_FILE_CAPTION)
 PUBLIC_FILE_STORE = is_enabled(environ.get("PUBLIC_FILE_STORE", "True"), True)
 
 # ---------- Verification ----------
-VERIFY_MODE = is_enabled(environ.get("VERIFY_MODE", "True"), True)
-SHORTLINK_URL = environ.get("SHORTLINK_URL", "get2short.com")  # without https://
-SHORTLINK_API = environ.get("SHORTLINK_API", "9ae188a4e5609c6f089d21d01816974230a64218")
-VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "https://t.me/VenomStoneNetwork/85")
+VERIFY_MODE = is_enabled(environ.get("VERIFY_MODE", "False"), False)
+SHORTLINK_URL = environ.get("SHORTLINK_URL", "")  # without https://
+SHORTLINK_API = environ.get("SHORTLINK_API", "")
+VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "")
 
 # ---------- Website Mode (for genlink.py) ----------
-WEBSITE_URL_MODE = is_enabled(environ.get("WEBSITE_URL_MODE"), False)
+WEBSITE_URL_MODE = is_enabled(environ.get("WEBSITE_URL_MODE"), True)
 WEBSITE_URL = environ.get("WEBSITE_URL", "")  # If enabled, set full base URL
 
 # ---------- File Stream Config ----------
@@ -92,4 +92,4 @@ SLEEP_THRESHOLD = int(environ.get("SLEEP_THRESHOLD", "60"))
 PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
 
 ON_HEROKU = "DYNO" in environ
-URL = environ.get("URL", "https://testofvjfilter-1fa60b1b8498.herokuapp.com/")-------------------------------------------------------------
+URL = environ.get("URL", "")-------------------------------------------------------------
